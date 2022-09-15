28044719c8aaa6cab59feda24566894145da3dec
LucasZuq <lucasdeozuquetto@hotmail.com>
2022-09-15 19:54:18 -0300

    criando token e inserindo na coleção de sessoes

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,13 +1,23 @@
 import db from "../src/db.js";
+import { v4 as uuid } from "uuid";
 
 export async function loginController(req, res) {
+   const token = uuid();
    const userData = req.body;
    try {
-      await db.collection("users").findOne({name:'123'});
-      console.log(userData)
-      res.send(userData)
+      const user = await db
+         .collection("users")
+         .findOne({ name: userData.name });
+      if (!user) {
+         return res.sendStatus(404);
+      }
+      await db.collection("sessions").insertOne({
+         userId: user._id,
+         token,
+      });
+      res.send(token);
    } catch (error) {
-    console.log(error.message)
-    res.sendStatus(500)
+      console.log(error.message);
+      res.sendStatus(500);
    }
 }
