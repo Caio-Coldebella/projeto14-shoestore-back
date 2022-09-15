03e4b739ca95a0d5b0b56e05ccb7efeadb890941
Caio-Coldebella <c232621@dac.unicamp.br>
2022-09-15 18:43:05 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ app.get('/', (req,res)=>{
     res.status(200).send("Hello world");
 });
 
-/*app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
-});*/
-app.listen(5000, ()=>{
-    console.log("Server running on port 5000")
 });
+/*app.listen(5000, ()=>{
+    console.log("Server running on port 5000")
+});*/
