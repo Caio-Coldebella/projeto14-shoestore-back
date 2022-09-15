66abe1d391e4c1d2d7df731815fcd29bde570022
Caio-Coldebella <c232621@dac.unicamp.br>
2022-09-14 21:29:17 -0300

    chore: adjust code for deploy on heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,6 @@ app.get('/', (req,res)=>{
     res.status(200).send("Hello world");
 });
 
-app.listen(5000, ()=>{
-    console.log("Server running on port 5000")
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
 });
