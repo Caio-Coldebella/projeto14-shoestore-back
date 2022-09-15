53f64ae081df3b54b7183944b8f89fe9ebf44e92
Caio-Coldebella <c232621@dac.unicamp.br>
2022-09-15 17:51:24 -0300

    finalização da rota post items, divisao em routes, controllers e middlewares

--- routers/itemsrouter.js
+++ routers/itemsrouter.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { itemspostcontroller, itemsgetcontroller } from "../controllers/itemscontroller.js";
-
+import itemMiddleware from "../middlewares/itemMiddleware.js";
 const itemsrouter = Router();
-itemsrouter.post('/items',itemspostcontroller);
+itemsrouter.post('/items',itemMiddleware,itemspostcontroller);
 itemsrouter.get('/items',itemsgetcontroller);
 export default itemsrouter;
