ebea024154015c8521c4d07d143198e41d439993
LucasZuq <lucasdeozuquetto@hotmail.com>
2022-09-19 09:17:37 -0300

    remoção usuarios inativos

--- middlewares/signupMiddleware.js
+++ middlewares/signupMiddleware.js
@@ -1,26 +1,29 @@
 import Joi from "joi";
-import db from "../src/db.js"
+import db from "../src/db.js";
 
-export default async function signupMiddleware(req,res,next){
-    const userData = req.body;
-    const postSchema = Joi.object({
-        name: Joi.string().min(1).required(),
-        email: Joi.string().email({ tlds: { allow: false } }),
-        password: Joi.string().min(1).required(),
-        address: Joi.string().min(1).required(),
-        phone: Joi.number().required()
-    });
-    const validate = postSchema.validate(userData);
-    if(validate.error){
-        res.sendStatus(400);
-        return;
-    }
-    const hasuser = await db.collection("users").find({name: userData.name}).toArray();
-    if(hasuser.length > 0){
-        res.sendStatus(400);
-        return;
-    }
+export default async function signupMiddleware(req, res, next) {
+   const userData = req.body;
+   const postSchema = Joi.object({
+      name: Joi.string().min(1).required(),
+      email: Joi.string().email({ tlds: { allow: false } }),
+      password: Joi.string().min(1).required(),
+      address: Joi.string().min(1).required(),
+      phone: Joi.number().required(),
+   });
+   const validate = postSchema.validate(userData);
+   if (validate.error) {
+      res.status(400).send(validate.error);
+      return;
+   }
+   const hasuser = await db
+      .collection("users")
+      .find({ email: userData.email })
+      .toArray();
+   if (hasuser.length > 0) {
+      res.status(400).send("Esse email já está cadastrado");
+      return;
+   }
 
-    res.locals.data = userData;
-    next();
+   res.locals.data = userData;
+   next();
 }
